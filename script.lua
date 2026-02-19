4d0d04971810a4561d929b53216d66d9754e168a
710200999 <198722707+710200999@users.noreply.github.com>
2026-02-19 13:54:36 +0700

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,14 @@
+loadstring(game:HttpGet"https://raw.githubusercontent.com/710200999/HixBox-_-BloxFruit/refs/heads/main/HixBox.lua")() -- hixbox
+
+loadstring(game:HttpGet"https://raw.githubusercontent.com/710200999/notification/refs/heads/main/notification.lua")() -- notification
+
+print("status : 🟢")
+
+
+
+--[[                                      
+/usr/local/lua-5.3.5/lua53: script.lua:1: attempt to index a nil value (global 'game')
+stack traceback:
+	script.lua:1: in main chunk
+	[C]: in ?
+]]--
